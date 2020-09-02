4a34dc5293251ae0683775bdbb90dcec1c3eb858
Luyaochen1 <lychen9999@gmail.com>
2020-09-02 14:16:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from distutils.core import setup
 setup(
   name = 'icd2phenotype',        
   packages = ['icd2phenotype'],  
-  version = '0.12',      
+  version = '0.13',      
   license='MIT',        
   description = 'Identify phenotypes from ICD code list',   
   include_package_data=True,
   author = 'Xiaoqian Jiang, Luyao Chen',                  
   author_email = 'your.email@domain.com',      
   url = 'https://github.com/Luyaochen1/icd2phenotype',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Luyaochen1/icd2phenotype/archive/V_012.tar.gz',     
+  download_url = 'https://github.com/Luyaochen1/icd2phenotype/archive/V_013.tar.gz',     
   keywords = ['icd 9', 'icd 10', 'phenotypes'],   
   install_requires=[            # I get to this in a second
           'pandas',
         ],
   classifiers=[
     'Development Status :: 3 - Alpha',       
-    'Intended Audience :: Bioinformatic Developers',       
+    'Intended Audience :: Developers',       
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3',      
